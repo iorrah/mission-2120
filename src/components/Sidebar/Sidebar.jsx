1b878649f04f8d1fd15516db83f3639551570937
Iorrah Mota <iorrah.gonzalez@gmail.com>
2020-01-26 10:51:27 +0100

    Add Sidebar stying

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -3,7 +3,17 @@ import React from "react";
 const Sidebar = function() {
   return (
     <aside className="sidebar">
-      <div className="sidebar__content">...</div>
+      <div className="sidebar__skeleton"></div>
+
+      <div className="sidebar__content">
+        <img src="img/icon-2.png" className="sidebar__icon" alt="Dashboard" />
+        <img src="img/icon-3.png" className="sidebar__icon" alt="Dashboard" />
+        <img src="img/icon-9.png" className="sidebar__icon" alt="Dashboard" />
+        <img src="img/icon-4.png" className="sidebar__icon" alt="Dashboard" />
+        <img src="img/icon-5.png" className="sidebar__icon" alt="Dashboard" />
+        <img src="img/icon-6.png" className="sidebar__icon" alt="Dashboard" />
+        <img src="img/icon-7.png" className="sidebar__icon" alt="Dashboard" />
+      </div>
     </aside>
   );
 };
