0ceaefc6dbbf71cd3568e01fe77e99075a3ad807
Iorrah Mota <iorrah.gonzalez@gmail.com>
2020-01-26 12:24:28 +0100

    Randomize mock data

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,94 +1,94 @@
 const consumers = [
   {
     id: 1,
-    name: "Martian Firma",
-    budget: "10000.0000",
-    budget_spent: "10000.0000",
-    date_of_first_purchase: "2119-07-07"
+    name: "Starbucks",
+    budget: "94726.5720",
+    budget_spent: "5783.7102",
+    date_of_first_purchase: "2119-03-16"
   },
   {
     id: 2,
-    name: "Martian Firma",
-    budget: "10000.0000",
-    budget_spent: "10000.0000",
-    date_of_first_purchase: "2119-07-07"
+    name: "NBC",
+    budget: "56104.0294",
+    budget_spent: "3943.5729",
+    date_of_first_purchase: "2119-09-20"
   },
   {
     id: 3,
-    name: "Martian Firma",
-    budget: "10000.0000",
-    budget_spent: "10000.0000",
-    date_of_first_purchase: "2119-07-07"
+    name: "Dominos",
+    budget: "17573.6610",
+    budget_spent: "6852.0139",
+    date_of_first_purchase: "2119-01-21"
   },
   {
     id: 4,
-    name: "Martian Firma",
-    budget: "10000.0000",
-    budget_spent: "10000.0000",
-    date_of_first_purchase: "2119-07-07"
+    name: "Shell",
+    budget: "94624.3092",
+    budget_spent: "1049.9294",
+    date_of_first_purchase: "2119-12-30"
   },
   {
     id: 5,
-    name: "Martian Firma",
-    budget: "10000.0000",
-    budget_spent: "10000.0000",
-    date_of_first_purchase: "2119-07-07"
+    name: "Pepsi",
+    budget: "46105.3610",
+    budget_spent: "2934.5724",
+    date_of_first_purchase: "2119-07-14"
   },
   {
     id: 6,
-    name: "Martian Firma",
-    budget: "10000.0000",
-    budget_spent: "10000.0000",
-    date_of_first_purchase: "2119-07-07"
+    name: "Dove",
+    budget: "89240.4619",
+    budget_spent: "5823.4991",
+    date_of_first_purchase: "2119-10-07"
   },
   {
     id: 7,
-    name: "Martian Firma",
-    budget: "10000.0000",
-    budget_spent: "10000.0000",
-    date_of_first_purchase: "2119-07-07"
+    name: "Dell",
+    budget: "30194.1613",
+    budget_spent: "9494.3966",
+    date_of_first_purchase: "2119-01-07"
   },
   {
     id: 8,
-    name: "Martian Firma",
-    budget: "10000.0000",
-    budget_spent: "10000.0000",
-    date_of_first_purchase: "2119-07-07"
+    name: "Fanta",
+    budget: "35124.1049",
+    budget_spent: "4935.1464",
+    date_of_first_purchase: "2119-04-02"
   },
   {
     id: 9,
-    name: "Martian Firma",
-    budget: "10000.0000",
-    budget_spent: "10000.0000",
-    date_of_first_purchase: "2119-07-07"
+    name: "LG",
+    budget: "64948.1485",
+    budget_spent: "4158.4957",
+    date_of_first_purchase: "2119-03-19"
   },
   {
     id: 10,
-    name: "Martian Firma",
-    budget: "10000.0000",
-    budget_spent: "10000.0000",
-    date_of_first_purchase: "2119-07-07"
+    name: "Volkswagen",
+    budget: "10353.3030",
+    budget_spent: "9485.7635",
+    date_of_first_purchase: "2119-06-15"
   },
   {
     id: 11,
-    name: "Martian Firma",
-    budget: "10000.0000",
-    budget_spent: "10000.0000",
-    date_of_first_purchase: "2119-07-07"
+    name: "Adidas",
+    budget: "12833.8845",
+    budget_spent: "5772.5588",
+    date_of_first_purchase: "2119-10-03"
   },
   {
     id: 12,
-    name: "Martian Firma",
-    budget: "10000.0000",
-    budget_spent: "10000.0000",
-    date_of_first_purchase: "2119-07-07"
+    name: "Cisco",
+    budget: "42952.0296",
+    budget_spent: "6620.0029",
+    date_of_first_purchase: "2119-12-12"
   },
   {
     id: 13,
-    name: "Martian Firma",
-    budget: "10000.0000",
-    budget_spent: "10000.0000",
-    date_of_first_purchase: "2119-07-07"
+    name: "Airbnb",
+    budget: "91835.6295",
+    budget_spent: "9246.2153",
+    date_of_first_purchase: "2119-05-08"
   }
 ];
 
