ca7a500b84af28a3094e588f6626d4b37a91520b
Iorrah Mota <iorrah.gonzalez@gmail.com>
2020-01-27 15:56:12 +0100

    Updates

---
+++ precache-manifest.2a7951d33ad80b9e5a01f417c975f127.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5baaad367261267d8ec9339c9b0b6b4",
+    "url": "/mission-2120/index.html"
+  },
+  {
+    "revision": "dadba4f14b2229b9ef88",
+    "url": "/mission-2120/static/css/2.82caf9b5.chunk.css"
+  },
+  {
+    "revision": "fadcabdcdbd3cab1b922",
+    "url": "/mission-2120/static/css/main.2cc63c88.chunk.css"
+  },
+  {
+    "revision": "dadba4f14b2229b9ef88",
+    "url": "/mission-2120/static/js/2.14ff5590.chunk.js"
+  },
+  {
+    "revision": "0c647f1dc701d286978d7a4e44c8c5b0",
+    "url": "/mission-2120/static/js/2.14ff5590.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fadcabdcdbd3cab1b922",
+    "url": "/mission-2120/static/js/main.0e8aba89.chunk.js"
+  },
+  {
+    "revision": "2167a464150a7d48bae6",
+    "url": "/mission-2120/static/js/runtime-main.97848eaa.js"
+  }
+]);
