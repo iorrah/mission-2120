ea25f6c9d78ba3eaf5078243e4e081d88f9f3aee
Iorrah Mota <iorrah.gonzalez@gmail.com>
2020-01-27 10:39:09 +0100

    Test the App component

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,9 +1,38 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import { shallow, mount } from "enzyme";
+import App from "./App";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("App", () => {
+  test("should shallow correctly", () => {
+    expect(shallow(<App />)).toMatchSnapshot();
+  });
+
+  test("should mount correctly", () => {
+    expect(mount(<App />)).toMatchSnapshot();
+  });
+
+  test("should render correctly", () => {
+    expect(render(<App />)).toMatchSnapshot();
+  });
+
+  it("renders the `.App` div", () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(".App").exists()).toBeTruthy();
+  });
+
+  it("renders the `.main` section", () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(".main").exists()).toBeTruthy();
+  });
+
+  it("renders the `.Toastify` container", () => {
+    const wrapper = mount(<App />);
+    expect(wrapper.find(".Toastify").exists()).toBeTruthy();
+  });
+
+  it("renders the `.sidebar`", () => {
+    const wrapper = mount(<App />);
+    expect(wrapper.find(".sidebar").exists()).toBeTruthy();
+  });
 });
